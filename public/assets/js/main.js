922227df54880884d6b4fb89a19ff1ac2eafcef4
Brandon Lavigne <brandon@caavadesign.com>
2017-08-17 22:59:28 -0700

    make secondary page load local videos

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,16 +1,23 @@
 var socket;
 
+var localUrl = 'files:///home/pi/screens/public';
+
 var videos = [
 	'/assets/video/video-1.mp4',
 	'/assets/video/video-2.mp4',
 	'/assets/video/video-3.mp4'
-]
+];
 
 jQuery(document).ready(function($) {
+	console.log(window.location);
 	socket = io.connect(window.location.origin);
 
 	function ResponsePlayVideo( video_id ) {
-		video = { 'mp4': videos[ video_id ] };
+		var videoUrl = videos[ video_id ];
+		if ( '/secondary.html' === window.location.pathname ) {
+			videoUrl = localUrl + videos[ video_id ];
+		}
+		video = { 'mp4': videoUrl };
 		$('.bg-video').attr( 'data-bg-video', JSON.stringify(video) );
 		$('.bg-video').find('video').remove();
 		console.log( $('.bg-video').attr('data-bg-video') );
